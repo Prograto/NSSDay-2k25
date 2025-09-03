e825ded45b57d2c656bd3e338425946f6e8f8cff
Arasavalli chandra sekhar <chanduarasavalli95@gmail.com>
2025-09-03 14:47:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({ hostname: 'https://nss-day-2k25.vercel.app' }) // Add the plugin here
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
